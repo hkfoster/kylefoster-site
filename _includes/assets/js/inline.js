7fd32daa48fed4822af8908b3a642e2b052c6195
Kyle Foster <hkylefoster@gmail.com>
2020-01-21 10:48:52 -0600

    Base tag view styles

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,3 +1,73 @@
+// Utils
+
+/**
+ * Throttle 0.0.1
+ * Event throttle function
+ * @author Kyle Foster (@hkfoster)
+ * @reference http://www.html5rocks.com/en/tutorials/speed/animations/
+ * @license MIT
+ **/
+
+// Public API function
+var throttle = function ( type, name, obj ) {
+  obj = obj || window;
+  var running = false;
+  var func = function () {
+    if ( running ) { return; }
+    running = true;
+    requestAnimationFrame( function () {
+      obj.dispatchEvent( new CustomEvent( name ) );
+      running = false;
+    });
+  };
+  obj.addEventListener( type, func );
+};
+
+/**
+ * Timeout 0.0.1
+ * Just like setTimeout & clearTimeout, but with requestAnimationFrame()
+ * @author Joe Lambert (@joelambert) & Kyle Foster (@hkfoster)
+ * @source https://gist.github.com/joelambert/1002116#file-requesttimeout-js
+ * @license MIT
+ **/
+
+// Public API object
+var timeout = {
+
+  // Set timeout function
+  set : function( fn, delay, args ) {
+
+    var start  = Date.now(),
+        handle = {};
+
+    function loop() {
+
+      var current = Date.now(),
+          delta   = current - start;
+
+      if ( delta >= delay ) {
+        if ( args !== undefined ) {
+          fn.call( fn, args );
+        } else {
+          fn.call( fn );
+        }
+      } else {
+        handle.value = requestAnimationFrame( loop );
+      }
+    }
+
+    handle.value = requestAnimationFrame( loop );
+    return handle;
+  },
+
+  // Clear timeout function
+  clear : function( handle ) {
+    window.cancelAnimationFrame( handle.value );
+  }
+};
+
+// Implementation
+
 /**
  * LoadFont 0.0.3
  * A font loader that leverages LocalStorage
@@ -70,6 +140,68 @@ var loadFont = function( fontName, fontFaceUrl, onlyLoadFontOnSecondPageload ){
 // Init
 loadFont('web-', '/styles/fonts.css')
 
+/**
+ * FullStop 0.0.2
+ * Prevent CSS transitions from occurring during a window resize
+ * @author Kyle Foster (@hkfoster)
+ * @license MIT
+ **/
+
+// Public API function
+var fullStop = function (settings) {
+
+  // Overridable defaults
+  var defaults = {
+    resizeDelay: 250,
+  };
+
+  // Scoped variables
+  var options = Object.assign({}, defaults, settings),
+    docBody = document.body,
+    resizeTimer;
+
+  // Resize handler function
+  function resizeHandler() {
+
+    // Clear timer (if it exists)
+    if (resizeTimer) timeout.clear(resizeTimer);
+
+    // Add style element while resizing
+    const fullStop = document.querySelector('#full-stop')
+
+    if (!fullStop) {
+      docBody.insertAdjacentHTML(
+        'beforeend',
+        `<style id="full-stop">
+          *, 
+          *:after, 
+          *:before { 
+            transition: none !important; 
+            animation: none !important; 
+          }
+        </style>`
+      );
+    }
+
+    // Check to see if resize is over
+    resizeTimer = timeout.set(function () {
+
+      // And style element upon completion
+      if (fullStop) docBody.removeChild(fullStop)
+
+      // Delay firing function based on argument passed
+    }, options.resizeDelay);
+
+  }
+
+  // Resize throttle function init
+  throttle('resize', 'optimizedResize');
+
+  // Resize function listener
+  window.addEventListener('optimizedResize', resizeHandler, false);
+
+};
+
 const ready = ( fn ) => {
   if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
     fn()
@@ -79,6 +211,11 @@ const ready = ( fn ) => {
 }
 
 ready(() => {
+  // This is pretty annoying
+  document.documentElement.focus();
+
+  fullStop();
+
   const menuButton = document.querySelector('.menu-button')
 
   menuButton.addEventListener('mousedown', toggleMenu)
